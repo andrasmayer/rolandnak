b8923a9a1affe64fe4d4d88ace1114f3f1c48157
unknown <mayerandras.weblap@gmail.com>
2023-10-20 21:58:56 +0200

    Feladat fele

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,3 +1,54 @@
+import React, {  useState, useRef } from "react";
+const loginData = {userName:"Admin",passWord:"admin"};
+
+
+
+
 export const Home = () => {
-  return <div>Home page</div>;
+
+  const [userData,setUserData] = useState({userName:null,passWord:null,loginStaus:false});
+
+
+
+  const handleUserName = (e) =>{ 
+     setUserData({...userData,userName:e.target.value});
+    
+  }
+  const handlePassword = (e) =>{  
+    setUserData({...userData,passWord:e.target.value});
+  }
+  const handleLogIn = (e) =>{
+    e.preventDefault();
+    console.log(userData);
+  }
+    
+  const LoginForm = () =>{
+
+    
+
+    
+  
+  }
+
+
+  return (<div className="col-3">
+    <h3>Home page</h3>
+    {
+      userData.loginStaus == false ? 
+      
+        
+      <form className="p-5 border rounded bg-light">
+        <h4>Kérlek jelentkezz be</h4>
+        <input className="form-control" placeholder="Felhasználónév" autoComplete="off" onChange={handleUserName}/>
+        <input className="form-control mt-2" placeholder="Jelszó" autoComplete="off"  type="password" onChange={handlePassword}/>
+        <div className="text-center p-2">
+          <button className="btn btn-primary" onClick={handleLogIn}>Belépés</button>
+        </div>
+      </form>
+     
+       : <div>Minden Oké</div>
+    }
+
+
+  </div>);
 };
--- src/pages/Lesson1/lesson1.js
+++ src/pages/Lesson1/lesson1.js
@@ -1,9 +1,108 @@
+import React, {  useState,useEffect  } from "react";
+
 const Page1 = () =>{
+
+  const [companyData,setCompanyData] = useState([]);
+  const [currentCompanyData,setCurrentCompanyData] =   useState({cmpName:"",cmpMail:"",cmpemps:"",cmpDesc:"",errors:true,dataSet:[]});
+
+  const handleCompanyName = (event) =>{ setCurrentCompanyData({...currentCompanyData,cmpName:event.target.value}) }
+  const handleCompanyMail = (event) =>{ setCurrentCompanyData({...currentCompanyData,cmpMail:event.target.value}) }
+  const handleCompanyEmps = (event) =>{ setCurrentCompanyData({...currentCompanyData,cmpemps:event.target.value}) }
+  const handleCompanyDesc = (event) =>{ setCurrentCompanyData({...currentCompanyData,cmpDesc:event.target.value}) }
+
+  const formValidator = () =>{
+      setCurrentCompanyData({...currentCompanyData,errors:
+        (
+          currentCompanyData.cmpName.length === 0 
+          || currentCompanyData.cmpMail.length === 0
+          || currentCompanyData.cmpemps.length === 0
+          || currentCompanyData.cmpDesc.length === 0
+        )
+      })
+  }
+
+ 
+  const handleSubmit = (event) =>{ 
+    event.preventDefault();
+
+    let companyClone = currentCompanyData.dataSet;
+
+ 
+    for(var i=0;i<currentCompanyData.cmpemps; i++){
+
+        let empData = {
+        cmpName:currentCompanyData.cmpName,
+        cmpMail:currentCompanyData.cmpMail,
+        cmpDesc:currentCompanyData.cmpDesc,
+        username:"",email:"",age:"",jobCode:"",file:""}
+
+        companyClone.push(empData);
+
+    }
+
+     setCurrentCompanyData({...currentCompanyData,dataSet:companyClone})
+  }
+
+
+
+
 
   return (
     <div>
-      <h1>Page 1</h1>
-      
+      {/* Company form*/ }
+      <form style={{border:"1px solid black",padding:20, borderRadius:10 }}>
+        <div>
+          <input placeholder="Company name" onChange={handleCompanyName} onKeyUp={formValidator}/>
+        </div>
+        <div>
+          <input placeholder="Company Email Address" onChange={handleCompanyMail} onKeyUp={formValidator}/>
+        </div>
+        <div>
+          <input placeholder="Number of employees" type="number" onChange={handleCompanyEmps} onKeyUp={formValidator}/>
+        </div>
+        <div>
+          <textarea placeholder="Description" onChange={handleCompanyDesc} onKeyUp={formValidator}/>
+        </div>
+        <button onClick={handleSubmit} disabled={currentCompanyData.errors}>Upload</button>
+      </form>
+
+<div>
+    {JSON.stringify(currentCompanyData) }
+
+</div>
+
+   
+  <table>
+    <thead>
+      <tr>
+        <th>Employee name</th>
+        <th>Employee Email Address</th>
+        <th>Employee age</th>
+        <th>Job description</th>
+        <th>Company</th>
+      </tr>
+    </thead>
+    <tbody>
+{
+currentCompanyData.dataSet.map((item,index)=>{
+return(
+<tr key={index}>
+  <td><input defaultValue={item.userName}/></td>
+  <td><input defaultValue={item.email}/></td>
+  <td><input defaultValue={item.age}/></td>
+  <td><input defaultValue={item.jobCode}/></td>
+  <td><input defaultValue={item.cmpName}/></td>
+
+</tr>
+)
+
+})
+
+}  
+
+      </tbody>
+  </table>
+
     </div>
   );
 }
